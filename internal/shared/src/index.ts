8c36b8c1b7e48218b9bfdedffef7de8c6cf152ad
Shahrad Elahi <shahrad@litehex.com>
2025-08-07 07:57:37 +0100

    fix linter issues

--- internal/shared/src/index.ts
+++ internal/shared/src/index.ts
@@ -1,1 +1,1 @@
-export * from 'src/enums';
+export * from './enums';
--- packages/websocket/src/modules/spot/ticker/types.ts
+++ packages/websocket/src/modules/spot/ticker/types.ts
@@ -1,7 +1,3 @@
-import { type SpotDepthData } from './depth';
-
-export type SpotDepth = SpotDepthData;
-
 export * from './state/types';
 export * from './depth/types';
 export * from './deals/types';
